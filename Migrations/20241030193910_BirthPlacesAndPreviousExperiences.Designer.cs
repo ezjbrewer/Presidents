c3f122e9c6a0a27147214a108ce9afdad7241552
Ezra Brewer <ezjbrewer@gmail.com>
2024-10-30 14:47:07 -0500

    Added additional seed data for BirthPlace and PreviousExperience entities

--- Migrations/20241030193910_BirthPlacesAndPreviousExperiences.Designer.cs
+++ Migrations/20241030193910_BirthPlacesAndPreviousExperiences.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Presidents.Data;
 
@@ -11,9 +12,11 @@ using Presidents.Data;
 namespace Presidents.Migrations
 {
     [DbContext(typeof(PresidentsDbContext))]
-    partial class PresidentsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241030193910_BirthPlacesAndPreviousExperiences")]
+    partial class BirthPlacesAndPreviousExperiences
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
