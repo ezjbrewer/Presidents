53b909c5c5d02fd63321f5a85339375934c4c7e4
Ezra Brewer <ezjbrewer@gmail.com>
2024-12-02 14:04:50 -0600

    Created Get A Single President Endpoint

--- Models/DTOs/PartyDTO.cs
+++ Models/DTOs/PartyDTO.cs
@@ -7,3 +7,23 @@ public class PartyDTO {
     public List<PersonDTO>? Presidents { get; set; }
     public List<PersonDTO>? VicePresidents{ get; set; }
 }
+
+public static class PartyDTOCreator
+{
+    public static PartyDTO ToPartyDTO(Party party)
+    {
+        if (party == null)
+        {
+            throw new ArgumentNullException(nameof(party));
+        }
+
+        PartyDTO partyDTO = new PartyDTO
+        {
+            Id = party.Id,
+            Name = party.Name,
+            Color = party.Color
+        };
+
+        return partyDTO;
+    }
+}
--- Models/DTOs/PreviousExperienceDTO.cs
+++ Models/DTOs/PreviousExperienceDTO.cs
@@ -7,7 +7,50 @@ public class PreviousExperienceDTO {
     public int PublicOfficeId { get; set; }
     public PublicOfficeDTO? PublicOffice { get; set; }
     public int StateId { get; set; }
-    public StateDTO? StateDTO { get; set; }
+    public StateDTO? State { get; set; }
     public DateTime StartDay { get; set; }
     public DateTime EndDay { get; set; }
+}
+
+public static class PreviousExperienceDTOCreator
+{
+    public static PreviousExperienceDTO ToPreviousExperienceDTO(PreviousExperience previousExperience, PublicOffice publicOffice, State state)
+    {
+        if (previousExperience == null)
+        {
+            throw new ArgumentNullException(nameof(previousExperience));
+        }
+
+        PublicOfficeDTO? publicOfficeDTO = publicOffice != null
+        ? new PublicOfficeDTO
+        {
+            Id = publicOffice.Id,
+            Role = publicOffice.Role
+        }
+        : null;
+
+        StateDTO? stateDTO = state != null
+        ? new StateDTO
+        {
+            Id = state.Id,
+            Name = state.Name,
+            CapitalName = state.CapitalName,
+            Population = state.Population
+        }
+        : null;
+
+        PreviousExperienceDTO previousExperienceDTO = new PreviousExperienceDTO
+        {
+            Id = previousExperience.Id,
+            PersonId = previousExperience.PersonId,
+            PublicOfficeId = previousExperience.PublicOfficeId,
+            PublicOffice = publicOfficeDTO,
+            StateId = previousExperience.StateId,
+            State = stateDTO,
+            StartDay = (DateTime)previousExperience.StartDay,
+            EndDay = (DateTime)previousExperience.EndDay
+        };
+
+        return previousExperienceDTO;
+    }
 }
